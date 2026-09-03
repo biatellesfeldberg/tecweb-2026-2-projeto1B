e02d1b84cb7e0ecc4c39e6b1376d2de04f7b90c1
biatellesfeldberg <beatrizfeldberg41@gmail.com>
2026-09-03 09:31:31 -0300

    Finalização Handout 4

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Note
+
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        note = Note(title=title, content=content)
+        note.save()
+
+        return redirect('index')
+    
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
